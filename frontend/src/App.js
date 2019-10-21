097be218ed014785422a1c741bf7a5ad4cf3405a
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-10-20 21:41:04 -0300

    add imports, get data from api, show data on screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import Crawl from 'react-star-wars-crawl'
+import moment from 'moment'
+import romannumerals from 'roman-numerals'
+
 import './App.css';
+import 'react-star-wars-crawl/lib/index.css'
+
+import logo from './assets/sw-logo.png'
 
 function App() {
+
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    async function loadFilms() {
+      const response = await api.get('/films', {
+      })
+      setFilms(response.data.results)
+    }
+    loadFilms()
+  }, [])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://swapi.co/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          SWAPI -The Star Wars API
-        </a>
+        <div className="App-intro">
+          <p>A long time ago in a galaxy far, far away...</p>
+        </div>
+        <img className="App-logo" src={logo} alt="logo" />
       </header>
+      <div>
+        <Crawl>
+          <ul>
+            {films.map(film => (
+              <div>
+                <p className="App-episode">Episode {romannumerals.toRoman(film.episode_id)}</p>
+                <p className="App-title">{film.title}</p>
+                <p className="App-release">Release Date: {moment(film.release_date).format('LL')}</p>
+                <p className="App-space">.</p>
+              </div>
+            ))}
+          </ul>
+        </Crawl>
+      </div>
     </div>
   );
 }
